a7eb659fc8583587b0c11bc1dd8fc63fa1fb30c0
Michael Jeffrey <michaeldjeffrey@gmail.com>
2013-06-12 16:04:11 -0700

    move Task object up in load order remove console log from TodoObject return localStorage as Tasks rather than objects

--- js/localStorage.js
+++ js/localStorage.js
@@ -5,7 +5,7 @@ function saveTask_localStorage (task) {
 function retrieve_localStorage(){
 	var a = [];
 	for ( var i = 0; i < localStorage.length; i++){
-		a.push(JSON.parse(localStorage.getItem(i)));
+		a.push(new Task(JSON.parse(localStorage.getItem(i))));
   }
   console.log('from retrieve localStorage',a);
   return a;
