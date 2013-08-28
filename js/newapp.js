134ee6b3be39c868faa43488289a4770159e000a
Michael Jeffrey <michaeldjeffrey@gmail.com>
2013-08-27 23:42:39 -0700

    save space

--- js/newapp.js
+++ js/newapp.js
@@ -2,18 +2,9 @@ var TASKS = localstorage_retrieve()
 var TAGS_ARRAY = []
 var STAR_IMPORTANCE_STATE = 0
 var STAR_IMPORTANCE = {
-    0: {
-        star: 'icon-star-empty',
-        text: 'Low Importance'
-    },
-    1: {
-        star: 'icon-star-half-empty',
-        text: 'Important'
-    },
-    2: {
-        star: 'icon-star',
-        text: 'Very Important'
-    }
+    0: {star: 'icon-star-empty', text: 'Low Importance'},
+    1: {star: 'icon-star-half-empty', text: 'Important'},
+    2: {star: 'icon-star', text: 'Very Important'}
 }
 var COMPLEX_TASK_INPUTS = [
     'title',
