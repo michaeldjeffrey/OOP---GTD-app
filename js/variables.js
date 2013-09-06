a024106db9fc56e06bf5bd5618e4fe3a3bb0a122
Michael Jeffrey <michaeldjeffrey@gmail.com>
2013-09-05 23:43:36 -0700

    move render strings to variables

--- js/variables.js
+++ js/variables.js
@@ -17,3 +17,24 @@ var COMPLEX_TASK_INPUTS = [
     'datepicker',
     'clockpick',
 ]
+//======================== TASK RENDERING =========================
+var TASK_ELEMENT =  "<div class='accordion-group {completed_state}' data-id='{collapse_id}'> \
+                        <div class='accordion-heading'> \
+                            <a class='accordion-toggle' data-toggle='collapse' data-parent='#accordion2' href='#collapse_{collapse_id}'> \
+                                {title} \
+                                <i id='check_{collapse_id}' data-status='{completed_state}' class='{completed_state_class} checkStyle pull-left'></i> \
+                                <i id='item_star_{collapse_id}' data-importance='{importance}' class='{priority} starStyle pull-left'></i> \
+                                <div class='sepLine pull-left'></div> \
+                                <input class='pull-left hide' type='checkbox' data-id={collapse_id}> \
+                                <span class='pull-right'>{due_date}</span> \
+                            </a> \
+                        </div> \
+                        {collapse} \
+                    </div>";
+var COLLAPSE_ELEMENT = "<div id='collapse_{collapse_id}' class='accordion-body collapse'> \
+                            <div class='accordion-inner'> \
+                                {description} \
+                                {tags} \
+                                {subtasks} \
+                             </div> \
+                        </div>";
