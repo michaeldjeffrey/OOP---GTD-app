8dcaba09a25140bb43d8ff153cde7bd7a5d705dc
Michael Jeffrey <michaeldjeffrey@gmail.com>
2013-09-05 21:18:19 -0700

    move global variables to own file

--- js/newapp.js
+++ js/newapp.js
@@ -1,23 +1,4 @@
 var TASKS = localstorage_retrieve();
-var AUTOCOMPLETE_TAGS = [];
-var TASK_STATUS = {
-    'completed': 'icon-check-sign',
-    'incomplete': 'icon-check-empty',
-}
-var TAGS_ARRAY = []
-var STAR_IMPORTANCE_STATE = 0
-var STAR_IMPORTANCE = {
-    0: {star: 'icon-star-empty', text: 'Low Importance'},
-    1: {star: 'icon-star-half-empty', text: 'Important'},
-    2: {star: 'icon-star', text: 'Very Important'}
-}
-var COMPLEX_TASK_INPUTS = [
-    'text',
-    'description',
-    'tags',
-    'datepicker',
-    'clockpick',
-]
 $(function(){
 //====================== MAKE TASKS FROM LOCALSTORAGE ========================
     for(task in TASKS){
